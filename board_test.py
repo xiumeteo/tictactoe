fb275fabb34d065bebb4682a7736761482f9e6d3
cricardez <cricardez@nearsoft.com>
2018-12-23 21:27:44 +0000

    minimax happy path

--- board_test.py
+++ board_test.py
@@ -1,4 +1,4 @@
-from game import Game,Move,InvalidMoveException,MoveCompleted,human,computer,draw,Minimax
+from game import Game,Move,InvalidMoveException,MoveCompleted,human,computer,noone,Minimax,Choice
 from pytest import raises
 
 def test_blank_board():
@@ -27,23 +27,57 @@ def test_override_move_not_allowed():
 def test_valid_move():
     b = Game()
     m = Move(computer, 5)
-    assert b.move(m) == MoveCompleted('continue', draw, m)
+    assert b.move(m) == MoveCompleted(noone, m)
 
 def test_Minimax_next_move():
     b = Game()
-    m  = Minimax(b, human)
-    validMoves = set(map(lambda x: ''.join(x.game.board), m.next_moves(b, human)))
+    m = Minimax(b, human)
+    validMoves = set(map(lambda x: ''.join(x.game.board), m.next_moves(b, computer)))
     expectedMoves = {"X________" , "_X_______", "__X______", "___X_____", "____X____", "_____X___", "______X__", "_______X_", "________X"}
     assert expectedMoves == validMoves
 
-def test_Minimax_game():
-    game = Game()
-    game.board = list("OX_"
-                   "OXO"
-                   "XOX")
+def test_Game_determine_winner():
+    game = Game(list("OXX"
+                     "OXO"
+                     "XOX"))
+    assert game.determine_winner() == computer
+
+def test_Minimax_game_almost_complete():
+    game = Game(list("OX_"
+                     "OXO"
+                     "XOX"))
+    print("initial game = ", game)
     minimax = Minimax(game, computer)
-    move = minimax.do()
-    print(move)
-    assert move == MoveCompleted('continue', computer, Move(computer, 2))
+    choice = minimax.do()
+    print(choice)
+    assert choice.move == MoveCompleted(computer, Move(computer, 2))
+
+def test_Choice_ordering():
+    computer_choice = Choice(MoveCompleted(computer, Move(computer, 5)), Game())
+    human_choice = Choice(MoveCompleted(human, Move(human, 5)), Game())
+    noone_choice = Choice(MoveCompleted(noone, Move(computer, 5)), Game())
+    choices = [computer_choice, human_choice, noone_choice]
+
+    assert max(choices) == computer_choice
+    assert min(choices) == human_choice
+
+def test_Minimax_game_incomplete():
+    game = Game(list("_X_"
+                     "OX_"
+                     "XOX"))
+    #human asks for move
+    minimax = Minimax(game, computer)
+    choice = minimax.do()
+    game.move(choice.move.move_initiated)
+    #human
+    h_move = game.get_available_slots()[0]
+    game.move(Move(human, h_move))
+    #computer
+    minimax = Minimax(game, computer)
+    choice = minimax.do()
+    game.move(choice.move.move_initiated)
+
+    assert game.is_done()
+    assert game.determine_winner() == computer
 
 
--- game.py
+++ game.py
@@ -1,74 +1,132 @@
-from dataclasses import dataclass, field
 from copy import deepcopy
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Player:
-    piece:str
-    value:int
+    piece: str
+    value: int
+
 
 human = Player('O', -10)
 computer = Player('X', 10)
-draw = Player('_', 0)
+noone = Player('_', 0)
 
-@dataclass 
+
+@dataclass
 class InvalidMoveException(Exception):
     reason: str
-    pos:int
+    pos: int
 
 
 @dataclass
 class Move:
-    player:Player
-    pos:int
+    player: Player
+    pos: int
 
     def is_machine(self):
         return self.player == computer
 
-@dataclass
-class MoveCompleted:
-    status:str
-    winner:Player
-    move:Move
 
 @dataclass
+class MoveCompleted:
+    winner: Player
+    move_initiated: Move
+
+
 class Game:
-    win_pos = {'XXX______', '___XXX___', '_____XXX', 
-            'X__X__X__', '_X__X__X_', '__X__X__X',
-            'X___X___X', '__X_X_X__'}
-    los_pos = {'OOO______', '___OOO___', '_____OOO', 
-            'O__O__O__', '_O__O__O_', '__O__O__O',
-            'O___O___O', '__O_O_O__'}
-    board = list("_________")
-    
+    def __init__(self, board=None):
+        if board is None:
+            self.board = list("_________")
+            self.board_values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        else:
+            self.board = board
+            self.board_values = list(map(self.__to_value, self.board))
+
+        self.winner_row_positions = [(0, 3), (3, 6), (6, 9)]
+        self.winner_column_positions = [(0, 3, 6), (1, 4, 7), (2, 5, 8)]
+        self.winner_diag_positions = [(0, 4, 8), (2, 4, 6)]
+
+    def __to_value(self, piece):
+        if piece == computer.piece:
+            return computer.value
+        if piece == human.piece:
+            return human.value
+        else:
+            return noone.value
+
     def move(self, move):
         if self.is_done():
-            return MoveCompleted('continue', self.determine_winner(), move)
+            return MoveCompleted(self.determine_winner(), move)
         if move.pos > 8 or move.pos < 0:
             raise InvalidMoveException("Out of range move", move.pos)
         if self.board[move.pos] is not '_':
             raise InvalidMoveException("Overriding move on: " + self.board[move.pos], move.pos)
         self.board[move.pos] = move.player.piece
-        return MoveCompleted('continue', self.determine_winner(), move)
+        self.board_values[move.pos] = move.player.value
+        return MoveCompleted(self.determine_winner(), move)
 
     def is_done(self):
         return not set(self.board).issuperset({'_'})
 
     def determine_winner(self):
-        string_board = {''.join(self.board)}
-        if self.win_pos.issuperset(string_board):
-            return computer
-        if self.los_pos.issuperset(string_board):
-            return human
-        return draw
+        for x, y in self.winner_row_positions:
+            total = sum(self.board_values[x:y])
+            winner = self.__check_winner(total)
+            if winner != noone:
+                return winner
 
-@dataclass(order=True)
+        for x, y, z in self.winner_column_positions:
+            total = self.board_values[x] + \
+                    self.board_values[y] + \
+                    self.board_values[z]
+            winner = self.__check_winner(total)
+            if winner != noone:
+                return winner
+
+        for x, y, z in self.winner_diag_positions:
+            total = self.board_values[x] + \
+                    self.board_values[y] + \
+                    self.board_values[z]
+            winner = self.__check_winner(total)
+            if winner != noone:
+                return winner
+
+        return noone
+
+    def __check_winner(self, value):
+        if value == computer.value * 3:
+            return computer
+        if value == human.value * 3:
+            return human
+        return noone
+
+    def get_available_slots(self):
+        slots = []
+        for i, item in enumerate(self.board_values):
+            if item == 0:
+                slots.append(i)
+        return slots
+
+    def __str__(self):
+        return ''.join(self.board)
+
+
+@dataclass
 class Choice:
     sort_index: int = field(init=False, repr=False)
-    move:MoveCompleted
-    game:Game
+    move: MoveCompleted
+    game: Game
 
     def __post_init__(self):
         self.sort_index = self.move.winner.value
+
+    def __lt__(self, other):
+        return self.sort_index < other.sort_index
+
+    def __gt__(self, other):
+        return self.sort_index > other.sort_index
+
 
 class Minimax:
     def __init__(self, currentBoard, currentPlayer):
@@ -76,34 +134,30 @@ class Minimax:
         self.current_player = currentPlayer
 
     def do(self):
-        return self.__deep(self.currentBoard, self.current_player)
+        return self.__deep(self.currentBoard, self.current_player)[0]
 
-    def __deep(self, currentBoard, currentPlayer):
-        #here we create the track of scores and positions
-        moves = self.next_moves(currentBoard, currentPlayer)
+    def __deep(self, current_board, current_player):
+        moves = self.next_moves(current_board, current_player)
         for choice in moves:
-            moves.extend(self.__deep(choice.game, choice.move))
+            moves.extend(self.__deep(choice.game, self.next_player(current_player)))
 
-        if moves == []:
+        if not moves:
             return moves
 
         if self.current_player == computer:
-            #maximize
-            return max(moves)
-        else :
-            return min(moves)
-
+            # maximize
+            return [max(moves)]
+        else:
+            return [min(moves)]
 
     def next_moves(self, current_board, current_player):
-        nextPlayer = self.next_player(current_player)
-
         board = deepcopy(current_board)
         valid_moves = []
         for pos in range(len(board.board)):
             piece = board.board[pos]
             if piece == '_':
                 futureBoard = deepcopy(board)
-                move = futureBoard.move(Move(nextPlayer, pos))
+                move = futureBoard.move(Move(current_player, pos))
                 valid_moves.append(Choice(move, futureBoard))
 
         return valid_moves
@@ -114,4 +168,3 @@ class Minimax:
         else:
             nextPlayer = human
         return nextPlayer
-
